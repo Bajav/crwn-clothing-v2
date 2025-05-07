33e0e6c95c3ffc24f4ccb2b30c93c192b4e62237
Balijawa Hussein <balijawahussein@gmail.com>
2025-05-07 16:21:42 +0300

    UPDATE : MAPPED DATA TO COMPONENT

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
 const App = () => {
+  const categories = [
+    { 
+      id: 1,
+      title: "hats",
+    },
+    { 
+      id: 2,
+      title: "jackets",
+    },
+    { 
+      id: 3,
+      title: "sweaters",
+    },
+    { 
+      id: 4,
+      title: "mens",
+    },
+    { 
+      id: 5,
+      title: "womens",
+    },
+  ];
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+    <div className="categories-container">
+      {categories.map((category) => (
+        <div className="category-container" key={category.id}>
+          {/* <img /> */}
+          <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
