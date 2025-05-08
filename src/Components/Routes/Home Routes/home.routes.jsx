2f7adb2ac490459d0750b0fca0d5413cda05baa0
Balijawa Hussein <balijawahussein@gmail.com>
2025-05-08 23:48:48 +0300

    UPDATE

--- src/Components/Routes/Home Routes/home.routes.jsx
+++ src/Components/Routes/Home Routes/home.routes.jsx
@@ -2,7 +2,8 @@ import { Routes, Route, Outlet } from "react-router-dom";
 // importing components
 import Directory from "../../Directory/directory.component";
 import NavBar from "../Navigation Routes/navigation.routes";
-
+import SignIn from "../signIn/signIn.component";
+import SignOut from "../signOut/signOut.component";
 const Shop = () => {
   return <h1>i am shop</h1>;
 };
@@ -14,8 +15,8 @@ function HomeRoute() {
       <Route path="/" element={<NavBar />}>
         <Route index element={<Directory />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="signIn" element={<Shop />} />
-        <Route path="signOut" element={<Shop />} />
+        <Route path="signIn" element={<SignIn />} />
+        <Route path="signOut" element={<SignOut />} />
       </Route>
     </Routes>
   );
