8ed5cfe66dae7cfa20d1e030d0f3ff060816c572
Balijawa Hussein <balijawahussein@gmail.com>
2025-05-08 17:06:12 +0300

    UPDATE : OUTLET

--- src/Components/Directory/directory.component.jsx
+++ src/Components/Directory/directory.component.jsx
@@ -3,12 +3,10 @@ import React, { Fragment } from "react";
 import CategoryItem from "../categories/category.item.component";
 import categories from "./directory.data.component";
 import "./directory.style.scss";
-import { Outlet } from "react-router-dom";
 
 function Directory() {
   return (
     <Fragment>
-      <Outlet />
       <div className="categories-container">
         {categories.map((category) => (
           <CategoryItem key={category.id} category={category} />
--- src/Components/Routes/Home Routes/home.routes.jsx
+++ src/Components/Routes/Home Routes/home.routes.jsx
@@ -1,14 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Directory from "../../Directory/directory.component";
+import { Fragment } from "react";
 
 const Shop = () => {
   return <h1>i am shop</h1>;
 };
 
+const NavBar = () => {
+  return (
+    <Fragment>
+      <div>
+        <h1>i am navbar</h1>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
 function HomeRoute() {
   return (
     <Routes>
-      <Route path="/" element={<Directory />}>
+      <Route path="/" element={<NavBar />}>
+        <Route index element={<Directory />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
