cb190fd74e133a7560769b7b6ba972c0055e3ed7
Balijawa Hussein <balijawahussein@gmail.com>
2025-05-17 01:04:30 +0300

    UPDATE : INPUTS FUNCTIONS

--- src/Components/sign-up-form/signUpForm.component.jsx
+++ src/Components/sign-up-form/signUpForm.component.jsx
@@ -1,17 +1,56 @@
 import { useState } from "react";
 import Input from "../Inputs/inputs.component";
-import './signUp.styles.scss';
+import "./signUp.styles.scss";
 
 function SignUpForm() {
+  const [inputs, setInputs] = useState({});
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs(values => ({...values, [name]: value}));
+    console.log(inputs);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
   return (
     <div className="form">
-        <h3>sign up</h3>
+      <h3>sign up</h3>
       <form action="">
-        <Input htmlFor="displayName" label="display name" type="text" placeholder="input displayName" name="displayName" change={()=>{}} />
-        <Input htmlFor="email" label="email" type="email" placeholder="enter email address" name="email" change={()=>{}} />
-        <Input htmlFor="password" label="password" type="password" placeholder="enter password" name="password" change={()=>{}} />
-        <Input htmlFor="co-password" label="co-password" type="password" placeholder="confirm password" name="co-password" change={()=>{}} />
-        <button type="submit">create account</button>
+        <Input
+          htmlFor="displayName"
+          label="display name"
+          type="text"
+          placeholder="input displayName"
+          name="displayName"
+          change={handleChange}
+        />
+        <Input
+          htmlFor="email"
+          label="email"
+          type="email"
+          placeholder="enter email address"
+          name="email"
+          change={handleChange}
+        />
+        <Input
+          htmlFor="password"
+          label="password"
+          type="password"
+          placeholder="enter password"
+          name="password"
+          change={handleChange}
+        />
+        <Input
+          htmlFor="co-password"
+          label="co-password"
+          type="password"
+          placeholder="confirm password"
+          name="co-password"
+          change={handleChange}
+        />
+        <button type="submit"  onSubmit={handleSubmit}>create account</button>
       </form>
     </div>
   );
