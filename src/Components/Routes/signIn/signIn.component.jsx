c57aa6a0b4f28fb6685299f68ac337be08b2fce9
Balijawa Hussein <balijawahussein@gmail.com>
2025-05-22 18:19:36 +0300

    CREATE : SIGN UP PAGES

--- src/Components/Routes/signIn/signIn.component.jsx
+++ src/Components/Routes/signIn/signIn.component.jsx
@@ -1,18 +1,6 @@
-import {
-  signInWithPop,
-  googleSignInRe,
-  createUserAuthFromDoc,
-} from "../../../utils/firebase.utils";
-import SignUpForm from "../../sign-up-form/signUpForm.component";
 import SignInForm from "../../sign-In-form/signIn.form";
 import './signIn.style.scss';
 function SignIn() {
-  const googlePopUp = async () => {
-    const { user } = await signInWithPop();
-    console.log("user uid::: ", user.uid);
-    const userDocRef = await createUserAuthFromDoc(user);
-    console.log(userDocRef);
-  };
 
   return (
     <section className="authPage">
---
+++ src/Components/SignUp/signUp.component.jsx
@@ -0,0 +1,12 @@
+import SignUpForm from "../sign-up-form/signUpForm.component";
+import 'signUp.styles.scss';
+
+function SignUp() {
+  return (
+    <div>
+        <SignUpForm />
+    </div>
+  )
+}
+
+export default SignUp
