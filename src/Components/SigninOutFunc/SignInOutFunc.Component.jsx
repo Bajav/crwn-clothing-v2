2d12908dcf3aaa372fb9becf7ad10f34c377c03b
Balijawa Hussein <balijawahussein@gmail.com>
2025-05-23 12:53:16 +0300

    UPDATE : UI EFFECTS

--- src/Components/SigninOutFunc/SignInOutFunc.Component.jsx
+++ src/Components/SigninOutFunc/SignInOutFunc.Component.jsx
@@ -1,21 +1,26 @@
 import "./SignInOut.styles.scss";
 import { Fragment, useState } from "react";
+import SignInForm from "../sign-In-form/signIn.form";
 
 function SignInOutFunc() {
-    const [isUserACtive,setUserAc] = useState(false);
-    const signIn =()=>{
-        alert("user signed in");
-        setUserAc(true);
-    }
-    const signOut =()=>{
-        alert("user signed out");
-        setUserAc(false);
-    }
+  const [isUserACtive, setUserAc] = useState(false);
+  const signIn = () => {
+    setUserAc(true);
+    return <SignInForm />;
+  };
+  const signOut = () => {
+    alert("user signed out");
+    setUserAc(false);
+  };
   return (
     <Fragment>
-    {!isUserACtive ?<button onClick={signIn}>Sign In</button> : <button onClick={signOut}>sign out</button>}
+      {!isUserACtive ? (
+        <button onClick={signIn}>Sign In</button>
+      ) : (
+        <button onClick={signOut}>sign out</button>
+      )}
     </Fragment>
-  )
+  );
 }
 
 export default SignInOutFunc;
