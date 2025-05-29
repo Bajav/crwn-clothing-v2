4106c9e8a0720f565679e45ed5daa683deef5b6a
Balijawa Hussein <balijawahussein@gmail.com>
2025-05-29 16:33:01 +0300

    UPDATE : PRODUCTS CARD

--- src/Components/Routes/Shop/shop.component.jsx
+++ src/Components/Routes/Shop/shop.component.jsx
@@ -1,20 +1,36 @@
-import { useContext } from "react";
+import { Fragment, useContext } from "react";
 import { ProductContext } from "../../Contexts/product.context";
-
+// import components
+import ProductCard from "../../productsCard/productsCard.component";
 const Shop = () => {
-  const {product} = useContext(ProductContext)
+  const { product } = useContext(ProductContext);
   return (
-    <div>
-      {product.map(({id,name})=>{
-        // console.log(id,name);
-       return(
-        <div key={id}>
-          <h1>{name}</h1>
-        </div>
-       )
+    <div className="shop page">
+      {product.map(({ id, name, imageUrl, price }) => {
+        // console.log(product);
+        return (
+          <Fragment>
+            <div key={id} className="card-container">
+              <div className="imageContainer">
+                <img src={imageUrl} />
+              </div>
+              <div className="functionsContainer">
+                <h4>{name}</h4>
+                <h4>{price}</h4>
+                <button>add to cart</button>
+              </div>
+            </div>
+          </Fragment>
+        );
       })}
     </div>
   );
 };
 
 export default Shop;
+//  <ProductCard
+//               key={id}
+//               name={name}
+//               imageUrl={imageUrl}
+//               price={price}
+//             />
---
+++ src/Components/productsCard/productsCard.component.jsx
@@ -0,0 +1,19 @@
+// style
+import "./products.card.scss";
+
+const ProductCard = (props) => {
+    // const {name,imageUrl,price} = product;
+    <div className="card-container">
+        <div className="imageContainer">
+            <img src={props.src} />
+        </div>
+        <div className="functionsContainer">
+            <h4>{props.name}</h4>
+            <h4>{props.price}</h4>
+            <button>add to cart</button>
+        </div>
+
+    </div>
+};
+
+export default ProductCard;
