9195b618e68198ba2b8665b1331a312cc42e8683
Balijawa Hussein <balijawahussein@gmail.com>
2025-05-29 15:31:35 +0300

    UPDATE : MAPPED SHOP DATA

--- src/Components/Routes/Shop/shop.component.jsx
+++ src/Components/Routes/Shop/shop.component.jsx
@@ -1,5 +1,19 @@
+import { Fragment } from "react/jsx-runtime";
+import shopData from "../../data/Shop.data.json";
+
+
 const Shop = () => {
-  return <h1>i am shop</h1>;
+  return (
+    <div>
+      {shopData.map(({id,name})=>{
+       return(
+        <div key={id}>
+          <h1>{name}</h1>
+        </div>
+       )
+      })}
+    </div>
+  );
 };
 
 export default Shop;
