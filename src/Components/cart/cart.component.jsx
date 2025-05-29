c4466c73baeb79ec9a942b54c9f0783a41e9f023
Balijawa Hussein <balijawahussein@gmail.com>
2025-05-29 22:34:01 +0300

    UPDATE : UI

--- src/Components/cart/cart.component.jsx
+++ src/Components/cart/cart.component.jsx
@@ -6,8 +6,7 @@ import CartItem from "../cartitem/cartItem.component";
 
 function Cart() {
   // state for cart
-  const [dropDown, setDropDown] = useState(false);
-  const [cartItems, setCartItems] = useState(0);
+  const [dropDown, setDropDown] = useState(true);
   // togle cart
   const togleCart = () => {
     console.log("cart clicked");
@@ -16,47 +15,19 @@ function Cart() {
   const closeCart = () => {
     setDropDown(false);
   };
-  const cartArray = [
-    {
-      id: 1,
-      itemName: "addidas dmn",
-      itemPrice: 300,
-    },
-    {
-      id: 2,
-      itemName: "addidas sambas",
-      itemPrice: 500,
-    },
-      {
-      id: 3,
-      itemName: "addidas sambas",
-      itemPrice: 500,
-    },
-      {
-      id: 4,
-      itemName: "addidas sambas",
-      itemPrice: 500,
-    },
-      {
-      id: 5,
-      itemName: "addidas sambas",
-      itemPrice: 500,
-    },
-  ];
-  useEffect(() => {
-    setCartItems(cartArray.length);
-  }, []);
+
   return (
     <div className="cartContainer" onClick={togleCart}>
       <h6>
-        cart <span>{cartItems}</span>
+        cart <span>{0}</span>
       </h6>
+      {dropDown &&
       <div className="dropDownCart">
         {cartArray.map(({ id,itemName,itemPrice}) => {
            return(<CartItem key={id} itemName={itemName} itemPrice={itemPrice}/>);
         })}
         <button onClick={closeCart}>close</button>
-      </div>
+      </div>}
     </div>
   );
 }
--- src/Components/cartitem/cartItem.component.jsx
+++ src/Components/cartitem/cartItem.component.jsx
@@ -2,14 +2,45 @@ import "./cartItem.styles.scss";
 import chair from "../images/image.jpg";
 
 function CartItem(item) {
+      const [cartItems, setCartItems] = useState(0);
+      const cartArray = [
+        {
+          id: 1,
+          itemName: "addidas dmn",
+          itemPrice: 300,
+        },
+        {
+          id: 2,
+          itemName: "addidas sambas",
+          itemPrice: 500,
+        },
+          {
+          id: 3,
+          itemName: "addidas sambas",
+          itemPrice: 500,
+        },
+          {
+          id: 4,
+          itemName: "addidas sambas",
+          itemPrice: 500,
+        },
+          {
+          id: 5,
+          itemName: "addidas sambas",
+          itemPrice: 500,
+        },
+      ];
+      useEffect(() => {
+        setCartItems(cartArray.length);
+      }, []);
   return (
     <div className="cartItem">
       <div className="imageContainer">
         <img src={chair} />
       </div>
       <div className="itemData">
-        <h1>{item.itemName}</h1>
-        <h4>2 x ${item.itemPrice}</h4>
+        <h1>{itemName}</h1>
+        <h4>2 x ${itemPrice}</h4>
       </div>
     </div>
   );
