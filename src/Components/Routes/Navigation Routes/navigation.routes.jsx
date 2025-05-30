a21805184d6ea09663b5c8726213e4662ae8fc2a
Balijawa Hussein <balijawahussein@gmail.com>
2025-05-30 14:07:11 +0300

    UPDATE : CART ITEM

--- src/Components/Routes/Navigation Routes/navigation.routes.jsx
+++ src/Components/Routes/Navigation Routes/navigation.routes.jsx
@@ -1,29 +1,30 @@
 import { Link, Outlet } from "react-router-dom";
 import { Fragment, useState } from "react";
-import './navgation.routes.styles.scss';
+import "./navgation.routes.styles.scss";
 import SignInOutFunc from "../../SigninOutFunc/SignInOutFunc.Component";
 import Cart from "../../cart/cart.component";
+import CartItem from "../../cartitem/cartItem.component";
 
 const NavBar = () => {
-  const {userActive,setUserAc} = useState(false);
-    return (
-      <Fragment>
-        <div className="nav">
+  const { userActive, setUserAc } = useState(false);
+  return (
+    <Fragment>
+      <div className="nav">
         <nav className="nav-items">
-            <Link to="">logo</Link>
-            <ul className="navlinks">
-                <Link to="/">home</Link>
-                <Link to="/shop">shop</Link>
-                <Link to="/signIn">sign In</Link>
-                <Link to="/signUp">sign UP</Link>
-            </ul>
-                <Cart />
-            
+          <Link to="">logo</Link>
+          <ul className="navlinks">
+            <Link to="/">home</Link>
+            <Link to="/shop">shop</Link>
+            <Link to="/signIn">sign In</Link>
+            <Link to="/signUp">sign UP</Link>
+          </ul>
+          <Cart />
         </nav>
-        </div>
-        <Outlet />
-      </Fragment>
-    );
-  };
+        <CartItem />
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
 
-  export default NavBar;
+export default NavBar;
